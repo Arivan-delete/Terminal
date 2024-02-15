e6f1fd8cd4e4c67f1b51c803884484516891949d
Artem <artem.mamedov5375@gmail.com>
2024-02-15 20:18:02 +0200

    Refactoring and bug fix

--- app/src/main/java/com/example/terminal/presentation/TerminalState.kt
+++ app/src/main/java/com/example/terminal/presentation/TerminalState.kt
@@ -12,32 +12,47 @@ import kotlin.math.roundToInt
 data class TerminalState(
     val barList: List<Bar>,
     val visibleBarsCount: Int = 100,
-    val terminalWidth: Float = 0f,
+    val terminalWidth: Float = 1f,
+    val terminalHeight: Float = 1f,
     val scrolledBy: Float = 0f
 ) {
     val barWidth: Float
         get() = terminalWidth / visibleBarsCount
 
-    val visibleBars: List<Bar>
+    private val visibleBars: List<Bar>
         get() {
             val startIndex = (scrolledBy / barWidth).roundToInt().coerceAtLeast(0)
             val endIndex = (startIndex + visibleBarsCount).coerceAtMost(barList.size)
             return barList.subList(startIndex, endIndex)
         }
 
+    val max: Float
+        get() = visibleBars.maxOf { it.high }
+    val min: Float
+        get() = visibleBars.minOf { it.low }
+    val pxPerPoint: Float
+        get() = terminalHeight / (max - min)
+
     companion object {
 
         val Saver: Saver<MutableState<TerminalState>, Any> = listSaver(
             save = {
                 val testData = it.value
-                listOf(testData.barList, testData.visibleBarsCount, testData.terminalWidth, testData.scrolledBy)
+                listOf(
+                    testData.barList,
+                    testData.visibleBarsCount,
+                    testData.terminalWidth,
+                    testData.scrolledBy,
+                    testData.terminalHeight
+                )
             },
             restore = {
                 val testData = TerminalState(
                     barList = it[0] as List<Bar>,
                     visibleBarsCount = it[1] as Int,
                     terminalWidth = it[2] as Float,
-                    scrolledBy = it[3] as Float
+                    scrolledBy = it[3] as Float,
+                    terminalHeight = it[4] as Float
                 )
                 mutableStateOf(testData)
             }
