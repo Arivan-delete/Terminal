dd9fca2a8b4c7909761426246adc8b546a39cbe5
Artem <artem.mamedov5375@gmail.com>
2024-02-15 16:52:02 +0200

    Data Loading

---
+++ app/src/main/java/com/example/terminal/data/ApiService.kt
@@ -0,0 +1,9 @@
+package com.example.terminal.data
+
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=M_XyMdWt5TsQfY_CdNoEYNqJJ1h9EvzQ")
+    suspend fun loadBars(): Result
+}
